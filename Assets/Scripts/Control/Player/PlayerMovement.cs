13005b96e6da2dc517f1c16e207a93aa5eb70a0e
YeuTeaKay <jhobabalos@gmail.com>
2026-04-18 22:37:59 +0800

    first commit

---
+++ Assets/Scripts/Control/Player/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace PlayerControl
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        private void FixedUpdate() 
+        {
+            
+        }
+
+        
+
+    }
+}
