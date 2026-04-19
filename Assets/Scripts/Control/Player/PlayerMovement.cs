1047c02c33bf2e3bb94af5bd870f43fb8cd8b267
YeuTeaKay <jhobabalos@gmail.com>
2026-04-20 02:35:32 +0800

    Added log for value movement

--- Assets/Scripts/Control/Player/PlayerMovement.cs
+++ Assets/Scripts/Control/Player/PlayerMovement.cs
@@ -1,17 +1,37 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace PlayerControl
 {
     public class PlayerMovement : MonoBehaviour
     {
 
+        [SerializeField] private InputActionReference moveAction;
+        private Vector2 moveInput;
+
+        private void Awake() 
+        {
+            moveAction.action.performed += OnMovePerformed;
+            moveAction.action.canceled += OnMoveCanceled;
+        }
+
+        private void OnMovePerformed(InputAction.CallbackContext context) 
+        {
+            moveInput = context.ReadValue<Vector2>();
+            Debug.Log("Move Input: " + moveInput);
+        }
+
+        private void OnMoveCanceled(InputAction.CallbackContext context)
+        {
+
+        }
+
         // Update is called once per frame
         private void FixedUpdate() 
         {
-            
-        }
 
-        
+        }
 
     }
 }
