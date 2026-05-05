72abc91610f1b44f3861c07affa92d1b8776a00f
YeuTeaKay <jhobabalos@gmail.com>
2026-05-05 10:30:17 +0800

    C# Wrapper

--- Assets/Scripts/Control/Player/PlayerMovement.cs
+++ Assets/Scripts/Control/Player/PlayerMovement.cs
@@ -4,34 +4,47 @@ using UnityEngine.InputSystem;
 
 namespace PlayerControl
 {
-    public class PlayerMovement : MonoBehaviour
+    public class PlayerMovement : MonoBehaviour, PlayerController.IOverWorldActions
     {
+        private PlayerController _input;
 
-        [SerializeField] private InputActionReference moveAction;
+        private Rigidbody rb;
         private Vector2 moveInput;
+        private Vector3 characterMove;
 
         private void Awake() 
         {
-            moveAction.action.performed += OnMovePerformed;
-            moveAction.action.canceled += OnMoveCanceled;
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+            _input = new PlayerController();
+            _input.OverWorld.SetCallbacks(this);
         }
 
-        private void OnMovePerformed(InputAction.CallbackContext context) 
+        private void OnDestroy()
+        {
+            _input.OverWorld.RemoveCallbacks(this);
+            _input.Dispose();
+        }
+    
+        private void OnEnable() => _input.Enable();
+        private void OnDisable() => _input.Disable();
+
+        // Update is called once per frame
+
+        void PlayerController.IOverWorldActions.OnMovement(InputAction.CallbackContext context)
         {
             moveInput = context.ReadValue<Vector2>();
             Debug.Log("Move Input: " + moveInput);
         }
-
-        private void OnMoveCanceled(InputAction.CallbackContext context)
-        {
-
-        }
-
-        // Update is called once per frame
         private void FixedUpdate() 
         {
-
+            OnMove();
+            rb.MovePosition(rb.position + characterMove * 5 * Time.fixedDeltaTime);
         }
 
+        private void OnMove()
+        {
+            characterMove = new Vector3(moveInput.x, 0, moveInput.y);
+        }
     }
 }
